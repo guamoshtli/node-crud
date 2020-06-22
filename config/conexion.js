da3f85196b094a62fc34843dba4cee74e7d20272
Gonzalo Sandoval <gonzalo.sandoval@amucss.org>
2020-06-22 04:09:57 -0500

    mlab pass change

--- config/conexion.js
+++ config/conexion.js
@@ -1,5 +1,7 @@
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb://username:password@port.mlab.com:15446/databasename', { useMongoClient: true }); //mongodb://localhost:27017/crud
+//mongoose.connect('mongodb://username:password@port.mlab.com:15446/databasename', { useMongoClient: true }); //mongodb://localhost:27017/crud
+//mongoose.connect('mongodb://localhost:27017/crud'); //mongodb://localhost:27017/crud
+mongoose.connect('mongodb://gmlab:Xio2009@ds059908.mlab.com:59908/gsrcrud01'); 
 
 module.exports = mongoose;
